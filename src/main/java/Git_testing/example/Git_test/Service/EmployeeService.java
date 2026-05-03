1aaf8c4ae8c8113615d38a7b5702245e7b4c8a46
Anjelina1998 <mohapatraanjelina@gmail.com>
2026-05-03 14:22:32 +0530

    my first commit

---
+++ src/main/java/Git_testing/example/Git_test/Service/EmployeeService.java
@@ -0,0 +1,7 @@
+package Git_testing.example.Git_test.Service;
+
+public class EmployeeService {
+    public static void SaveAllEmployee(){
+        System.out.println("employee saved");
+    }
+}
---
+++ src/test/java/Git_testing/example/Git_test/GitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package Git_testing.example.Git_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
