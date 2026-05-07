1b1656ee637204e0ca2b0fff940650ae33130b3f
Anjelina1998 <mohapatraanjelina@gmail.com>
2026-05-08 00:06:46 +0530

    children method added

---
+++ src/main/java/Git_testing/example/Git_test/Service/ChildrenService.java
@@ -0,0 +1,10 @@
+package Git_testing.example.Git_test.Service;
+
+public class ChildrenService {
+    public static void SaveAllChildren(){
+
+        System.out.println("children saved");
+    }
+}
+
+
