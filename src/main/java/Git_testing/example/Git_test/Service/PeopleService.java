5975f9f59c1527965b631c26c1c24662845ead89
Anjelina1998 <mohapatraanjelina@gmail.com>
2026-05-17 16:24:34 +0530

    add new changes

--- src/main/java/Git_testing/example/Git_test/Service/PeopleService.java
+++ src/main/java/Git_testing/example/Git_test/Service/PeopleService.java
@@ -1,4 +1,8 @@
 package Git_testing.example.Git_test.Service;
 
 public class PeopleService {
+    public static void SaveAllPeople(){
+
+        System.out.println("people saved");
+    }
 }
--- src/main/java/Git_testing/example/Git_test/Service/StudentService.java
+++ src/main/java/Git_testing/example/Git_test/Service/StudentService.java
@@ -1,4 +1,8 @@
 package Git_testing.example.Git_test.Service;
 
 public class StudentService {
+    public static void SaveAllStudent(){
+        System.out.println("Student saved");
+    }
 }
+
